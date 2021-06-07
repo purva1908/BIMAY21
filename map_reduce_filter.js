981ee67b9fe5a89718b3d21dd679db05cb42645d
Rahul Kumar <rahul10.pu@gmail.com>
2021-06-07 09:46:46 +0530

    codes for function, class, array, mrf

---
+++ map_reduce_filter.js
@@ -0,0 +1,35 @@
+var arr=[1,2,3,4,5,6]
+// var prod=1
+// arr.forEach(num=>{
+//     prod=prod*num
+// })
+// console.log(prod) // 720
+
+// function findprod(a,b){
+//     console.log(a + " X "+ b)
+//     return a*b
+// }
+// var prod=arr.reduce(findprod)
+// console.log(prod)
+// for(let i=0;i<arr.length;i++){
+//     arr[i]=arr[i]*arr[i]
+// }
+// console.log(arr) 
+
+// function sqr(num){
+//     return num*num
+// }
+// console.log(arr.map(sqr))
+// arr.forEach(num=>{
+//     if(num%2==0){
+//         console.log(num)
+//     }
+// })
+ function isEven(num){
+    if(num%2==0){
+        return true
+    }else{
+        return false
+    }
+ }
+ console.log(arr.filter(isEven))
