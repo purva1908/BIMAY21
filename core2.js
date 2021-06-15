fcf7487ce6d32e6f778f229c4320d013f0852f63
Rahul Kumar <rahul10.pu@gmail.com>
2021-06-15 08:11:45 +0530

    cleaning repo

--- core2.js
+++
@@ -1,9 +0,0 @@
-var data=require("./core1")
-// console.log(data)
-//how to call printName function from 
-//core1.js
-// console.log(data.name)
-// console.log(data.age)
-// data.printName()
-// data.one()
-data()
